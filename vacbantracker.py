e0ec657b31a131e93cb358180a7f2070351a5e8c
Gregory Oh <gregoryoh0609@gmail.com>
2024-05-03 14:31:33 -0700

    Cleaned up VAC ban scraper

--- vacbantracker.py
+++ vacbantracker.py
@@ -7,32 +7,46 @@ from selenium import webdriver
 import time
 from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
 
-# Define your target URL
+
+class WebDriver(object):
+    def __init__(self):
+        self.options = Options()
+        
+        self.options.add_argument('--headless')
+    def get(self):
+        driver = Firefox(options=self.options)
+        return driver
+
+def hover_action(driver):
+    time.sleep(2)
+    unhide_element = driver.find_element(By.ID,"chart_bans180")
+    hover_action = ActionChains(driver)
+    hover_action.move_to_element(unhide_element).perform()
+    hover_action.move_by_offset(450,0).perform()
+    time.sleep(2)
+    print("Completed hover action")
+
+def get_ban_info(driver):
+    ban_Info = driver.find_element(By.XPATH,"/html/body/main/div/div[4]/div/div")
+    return ban_Info
+        
+def decode_ban_info(data):
+    vac_Count = data.text.encode('ascii','ignore').decode('utf-8')
+    return vac_Count
+
+
 url = 'https://convars.com/csgostats/en/bans'
-#https://stackoverflow.com/questions/39305877/can-i-scrape-the-raw-data-from-highcharts-js how to get raw data from highcharts.js
-# raw data for vac bans and game bans are in tooltop. div class="highcharts-label highcharts-tooltip highcharts-color-undefined"
-# use selenium to put mouse at the most recent day, then extract the data from the tooltip
 
-options = webdriver.FirefoxOptions()
-options.add_argument("-headless")
-driver = webdriver.Firefox(options = options)
+instance_ = WebDriver()
+driver = instance_.get()
 driver.get(url)
-time.sleep(5)
-htmlSource = driver.page_source
-# Places cursor in middle of chart first to unhide hidden element that is responsible for the tooltip
-unhide_element_event = driver.find_element(By.ID,"chart_bans180")
-hover_action = ActionChains(driver)
-hover_action.move_to_element(unhide_element_event).perform()
-hover_action.move_by_offset(450,0).perform()
-
-time.sleep(2)
-ban_Info = driver.find_element(By.XPATH,"/html/body/main/div/div[3]/div/div/span")
-
-# encode data into ascii then decode that data into utf-8
-
-vac_Count = ban_Info.text.encode('ascii','ignore').decode('utf-8')
+hover_action(driver)
+ban_Info = get_ban_info(driver)
+vac_Count = decode_ban_info(ban_Info)
 print(vac_Count)
+driver.quit
 
 
-driver.quit()
