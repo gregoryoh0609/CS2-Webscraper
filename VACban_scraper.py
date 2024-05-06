65b938803e2f1e6ca9d165b81e20901182dfa18e
Gregory Oh <gregoryoh0609@gmail.com>
2024-05-05 23:04:45 -0700

    Organized for Lambda and both scrapers now return a dictionary

--- VACban_scraper.py
+++ VACban_scraper.py
@@ -20,34 +20,35 @@ class WebDriver(object):
         driver = Firefox(options=self.options)
         return driver
 
-def hover_action(driver):
+def hover_action(driver,x,y,location):
     time.sleep(2)
-    unhide_element = driver.find_element(By.ID,"chart_bans180")
+    unhide_element = driver.find_element(By.ID,location)
     hover_action = ActionChains(driver)
     hover_action.move_to_element(unhide_element).perform()
-    hover_action.move_by_offset(450,0).perform()
+    hover_action.move_by_offset(x,y).perform()
     time.sleep(3)
     print("Completed hover action")
 
 def get_ban_info(driver):
     # ban_Info = driver.find_element(By.XPATH,"/html/body/main/div/div[3]/div/div")
-    ban_Info = driver.find_element(By.XPATH,"//div[@class='highcharts-label highcharts-tooltip highcharts-color-undefined']")
-    return ban_Info
-        
-def decode_ban_info(data):
-    vac_Count = data.text.encode('ascii','ignore').decode('utf-8')
-    return vac_Count
+    # ban_Info = driver.find_element(By.XPATH,"//div[@class='highcharts-label highcharts-tooltip highcharts-color-undefined']")
+    date = driver.find_element(By.XPATH,"/html/body/main/div/div[4]/div/div/span/div[1]")
+    date = date.text.encode('ascii','ignore').decode('utf-8')
+    day = int(date[:2])
+    year = int(date[-2:])
+    month = date.split(' ')[1]
 
+    vac_num = driver.find_element(By.XPATH,"/html/body/main/div/div[4]/div/div/span/b[1]")
+    vac_num = vac_num.text.encode('ascii','ignore').decode('utf-8')
+    return {
+        'Month' : month,
+        'Day' : day,
+        'Year' : year,
+        'VAC_count' : vac_num
+        }
 
-url = 'https://convars.com/csgostats/en/bans'
-
-instance_ = WebDriver()
-driver = instance_.get()
-driver.get(url)
-hover_action(driver)
-ban_Info = get_ban_info(driver)
-vac_Count = decode_ban_info(ban_Info)
-print(vac_Count)
-driver.quit
-
+# depreciated function         
+# def decode_ban_info(data):
+#     vac_Count = data.text.encode('ascii','ignore').decode('utf-8')
+#     return vac_Count
 
